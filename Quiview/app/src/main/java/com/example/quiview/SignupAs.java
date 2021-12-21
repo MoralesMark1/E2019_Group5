18dee47c3d72faf8e7f787c262f1fe09aba58f63
MoralesMark1 <94729459+MoralesMark1@users.noreply.github.com>
2021-12-21 17:53:49 +0800

    UI Sign-Up added

---
+++ Quiview/app/src/main/java/com/example/quiview/SignupAs.java
@@ -0,0 +1,14 @@
+package com.example.quiview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupAs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_as);
+    }
+}
