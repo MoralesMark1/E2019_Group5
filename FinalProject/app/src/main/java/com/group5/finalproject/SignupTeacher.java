ffed00b52550fa17c18b1a74e7f682281f63d7a0
JrmPsg <94660514+JrmPsg@users.noreply.github.com>
2021-12-24 12:49:55 +0800

    Minor Changes

--- FinalProject/app/src/main/java/com/group5/finalproject/SignupTeacher.java
+++ FinalProject/app/src/main/java/com/group5/finalproject/SignupTeacher.java
@@ -5,13 +5,10 @@ import androidx.appcompat.widget.AppCompatButton;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.transition.Explode;
 import android.transition.Slide;
 import android.view.View;
 import android.view.Window;
