918abbc805c00293bf39a0751d039fdcf27ce554
MaricarRaci <94663009+verocali@users.noreply.github.com>
2021-12-30 13:16:55 +0800

    Teacher Home Page Added

---
+++ FinalProject/app/src/main/java/com/group5/finalproject/My_profile_student.java
@@ -0,0 +1,14 @@
+package com.group5.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class My_profile_student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile_student);
+    }
+}
