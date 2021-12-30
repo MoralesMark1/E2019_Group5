3f59d620d51595b273a4d8360696d0f46edac0bc
MoralesMark1 <94729459+MoralesMark1@users.noreply.github.com>
2021-12-30 18:33:52 +0800

    Classes Added

--- FinalProject/app/src/main/java/com/group5/finalproject/StudentHomePage.java
+++ FinalProject/app/src/main/java/com/group5/finalproject/StudentHomePage.java
@@ -1,14 +1,37 @@
 package com.group5.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StudentHomePage extends AppCompatActivity {
+
+    String []data = {"Mobile Programming", "Micro-controller", "Software Engineering"};
+    int cntr = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home_page);
+
+        List<String> items = new LinkedList<>();
+        items.add("Code here");
+
+        RecyclerView recyclerView = findViewById(R.id.recycleView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ClassesAdapter adapter = new ClassesAdapter(items);
+        recyclerView.setAdapter(adapter);
+
+        findViewById(R.id.join_class).setOnClickListener(view -> {
+            items.add(data[cntr%3]);
+            cntr++;
+            adapter.notifyItemInserted(items.size()-1);
+        });
     }
 }
