b68fe92961c0636d3869f557e48427133ceeb881
JrmPsg <94660514+JrmPsg@users.noreply.github.com>
2022-01-30 12:43:56 +0800

    Change Deprecated startActivityOnResult to Activity Results Launcher

--- FinalProject/app/src/main/java/com/group5/finalproject/TeacherHomePage.java
+++ FinalProject/app/src/main/java/com/group5/finalproject/TeacherHomePage.java
@@ -1,22 +1,39 @@
 package com.group5.finalproject;
 
+import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.transition.Slide;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -63,12 +80,24 @@ public class TeacherHomePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                /*
                 //newQuiz function po ditoooo
                 myfileIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 myfileIntent.setType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"); // Specific FIle format
                 myfileIntent.addCategory(Intent.CATEGORY_OPENABLE); // to open the file
                 startActivityForResult(myfileIntent, 10);
+                */
 
+                //Gagamit ako ng bagong startActivityForResult since deprecated na siya hayy kainis
+                //Pang open ito ng files natin
+                //Say Hello to Activity Results Launcher hayyyy Research pa moreeeee
+
+               myfileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+               myfileIntent.setType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+               myfileIntent.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+               myfileIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); //Read lang poo walang write
+                //Then yung ating hayyy yungggg activity results launcher
+                excelReader.launch(myfileIntent); //Para daw hindi deprecated tulad nung nakacomment sa taas
 
             }
         });
@@ -95,6 +124,7 @@ public class TeacherHomePage extends AppCompatActivity {
     //  ----------------------------- Excel Import Code -------------------------------
 //  --------------- Mga Pre Eto yung Code para sa importing @-Charlie -------------
 // ---------------- Dito yung path nung file --------------------------
+    /************** D E P R E C A T E D *****************************************
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
@@ -115,7 +145,81 @@ public class TeacherHomePage extends AppCompatActivity {
                 }
                 break;
         }
+        *******************************************************/
+    private ActivityResultLauncher<Intent> excelReader = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        Uri path_describer = data.getData();
+                        //Tingnan ko kung magkakaroon ba ng path using Log
+                        Log.d("ExcelPath: ", path_describer.getPath());
+                        String source = path_describer.getPath();
+                        File excelFile = new File(source); // Sana makuha na ang source absolute na directory or path
+                        Log.d("Source Path: ",excelFile.toString()); //Eh masaya magdebug sa logcat ehh mas madali
+                        String filename = path_describer.getLastPathSegment();
+                        Log.d("Excel Filename is ",filename); //Filename nung excel na naglalaman ng questions nandito sanaa ngaa
 
+                        //Then next is ang pagkuha nung file papunta saaa workbook
+                        /************************
+                         * Dito sa part na ito may function sa baba siguro
+                         * gagmaitin ko yun then ipapass ko yung File excel as parameter para makuha natin
+                         * since dito ang excelFile is may source path na siya which is sana... Absolute na
+                         */
+                        try {
+                            FileInputStream excelStream = new FileInputStream(excelFile.getAbsolutePath());
+                            //Hahaha pwede pala mag absolute path mismo dun sa FileInputStream ahhahaha nakakabaliw
+
+                            XSSFWorkbook workbook = new XSSFWorkbook(excelStream);
+
+                            XSSFSheet sheet = workbook.getSheetAt(0); //Sheetname sa baba ahh
+
+                            //Then susunod yung sa row number natin to get yung last row para makuha
+                            // sila lahat na ipapasok sa ating custom arraylist object then pag may null value then stop siya
+                            // which is obvious naman na ang null ay nandun sa dulo hayyy
+                            int nrow = sheet.getLastRowNum();
+
+                            for(int i = 0; i < nrow+1; i++){
+                                XSSFRow row = sheet.getRow(i);
+                                XSSFCell cellq = row.getCell(0); //Question
+                                XSSFCell cellqa = row.getCell(0); //Question Choice 1
+                                XSSFCell cellqb = row.getCell(0); //Question Choice 2
+                                XSSFCell cellqc = row.getCell(0); //Question Choice 3
+                                XSSFCell cellqd = row.getCell(0); //Question Choice 4
+                                XSSFCell cellans = row.getCell(0); //Question Answer Key
+
+                                //Then pasok sila every string natin na maghahawak ng value every for loop incrementation
+                                String q = String.valueOf(cellq);
+                                String qa = String.valueOf(cellqa);
+                                String qb = String.valueOf(cellqb);
+                                String qc = String.valueOf(cellqc);
+                                String qd = String.valueOf(cellqd);
+                                String ans = String.valueOf(cellans);
+
+                                //Then ipapasok natin siya sa ating custom arraylist hayyyyy buhayyyyyyy
+                                questions.add(new Questions(q,qa,qb,qc,qd,ans));
+
+                                for(Questions que: questions){
+
+                                    Log.i("TAG",que.getQuestion() + " | "
+                                            + que.getChoiceA() + " | " + que.getChoiceB() + " | "
+                                            + que.getChoiceC() + " | " + que.getChoiceD() + " | "
+                                            + que.getAnswer()
+                                    );
+                                }
+                            }
+                        }
+                        catch (FileNotFoundException e){
+                            e.printStackTrace();
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
 
 // ----------------------------- End of Import Excel Code---------------------------------
 
@@ -172,7 +276,6 @@ public class TeacherHomePage extends AppCompatActivity {
                         + que.getAnswer()
                 );
             }*/
-    }
     private void layouts(){
 
         //Lagay ako transition para maganda
